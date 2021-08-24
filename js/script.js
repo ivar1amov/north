ae215ba80965b4f71ab7596a7b16663d3a1d14f9
Andrey Varlamov <45011364+ivar1amov@users.noreply.github.com>
2021-08-25 00:13:31 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,19 +6,46 @@ var swiper = new Swiper(".mySwiper", {
  });
 
 
- window.onload = function() {
-  var oQuotes = document.querySelector('.slogan__inner ul.slogan__list');
+//  window.onload = function() {
+//   var oQuotes = document.querySelector('.slogan__inner ul.slogan__list');
 
-  /* Функция дублирования пунктов */
-  function fDoublingQuotes() {
-    let nElem = oQuotes.children.length;
-    for (let i = 0; i < nElem; i++) {
-      oQuotes.appendChild(oQuotes.children[i].cloneNode(true));
+//   /* Функция дублирования пунктов */
+//   function fDoublingQuotes() {
+//     let nElem = oQuotes.children.length;
+//     for (let i = 0; i < nElem; i++) {
+//       oQuotes.appendChild(oQuotes.children[i].cloneNode(true));
+//     }
+//     oQuotes.style.animationDuration = '30s,' + (nElem * 2) + 's';
+//   }
+
+
+
+//   fDoublingQuotes();
+// }
+
+
+
+
+
+$(document).ready(function() {
+  // Assign some jquery elements we'll need
+  var $swiper = $(".MySwipperTaste");
+  var $bottomSlide = null; // Slide whose content gets 'extracted' and placed
+  // into a fixed position for animation purposes
+  var $bottomSlideContent = null; // Slide content that gets passed between the
+  // panning slide stack and the position 'behind'
+  // the stack, needed for correct animation style
+
+  var mySwiper = new Swiper(".MySwipperTaste", {
+    spaceBetween: 30,
+    slidesPerView: 3,
+    centeredSlides: true,
+    roundLengths: true,
+    loop: true,
+    loopAdditionalSlides: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
     }
-    oQuotes.style.animationDuration = '30s,' + (nElem * 2) + 's';
-  }
-
-
-
-  fDoublingQuotes();
-}
+  });
+});
