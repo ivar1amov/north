2d928f5786d7559ccc40c14cd66646f2c6e43fe3
Andrey Varlamov <45011364+ivar1amov@users.noreply.github.com>
2021-08-27 02:39:55 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,6 +9,12 @@
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+	 breakpoints: {
+		600: {
+		  slidesPerView: 1,
+		  spaceBetween: 20,
+		},
+	 }
   });
 
 
@@ -29,13 +35,13 @@
 //   });
 
 
-const swiperTop = new Swiper(".slider-top", {
-	navigation: {
-	  nextEl: ".swiper-button-next",
-	  prevEl: ".swiper-button-prev",
-	},
-	loop: true,
- });
+// const swiperTop = new Swiper(".slider-top", {
+// 	navigation: {
+// 	  nextEl: ".swiper-button-next",
+// 	  prevEl: ".swiper-button-prev",
+// 	},
+// 	loop: true,
+//  });
 
  
  
@@ -98,3 +104,65 @@ function openMenu() {
 	$(`.${close2}`).removeClass("visivle").addClass("hidden");
  });
  
+
+ const swiperTopTop = new Swiper(".swiper-top", {
+	loop: true,
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+	slidesPerView: 1,
+ });
+
+
+
+ $('.js-tab-trigger').click(function() {
+	// клик! 
+})
+$('.js-tab-trigger').click(function() {
+  var id = $(this).attr('data-tab'), // 1
+	  content = $('.js-tab-content[data-tab="'+ id +'"]'); // 2
+});
+
+$('.js-tab-trigger').click(function() {
+  var id = $(this).attr('data-tab'),
+	  content = $('.js-tab-content[data-tab="'+ id +'"]');
+  
+  $('.js-tab-trigger.active').removeClass('active'); // 1
+  $(this).addClass('active'); // 2
+  
+  $('.js-tab-content.active').removeClass('active'); // 3
+  content.addClass('active'); // 4
+});
+
+
+var jsTriggers = document.querySelectorAll('.js-tab-trigger'),
+	jsContents = document.querySelectorAll('.js-tab-content');
+	
+	jsTriggers.forEach(function(trigger) { // 1
+  trigger.addEventListener('click', function() { // 2
+	 // клик!
+  });
+});
+var id = this.getAttribute('data-tab'), // 1
+	content = document.querySelector('.js-tab-content[data-tab="'+id+'"]'), // 2
+	activeTrigger = document.querySelector('.js-tab-trigger.active'), // 3
+	activeContent = document.querySelector('.js-tab-content.active'); // 4
+	
+	var jsTriggers = document.querySelectorAll('.js-tab-trigger'),
+	jsContents = document.querySelectorAll('.js-tab-content');
+
+jsTriggers.forEach(function(trigger) {
+  trigger.addEventListener('click', function() {
+	 var id = this.getAttribute('data-tab'),
+		 content = document.querySelector('.js-tab-content[data-tab="'+id+'"]'),
+		 activeTrigger = document.querySelector('.js-tab-trigger.active'),
+		 activeContent = document.querySelector('.js-tab-content.active');
+	 
+	 activeTrigger.classList.remove('active'); // 1
+	 trigger.classList.add('active'); // 2
+	 
+	 activeContent.classList.remove('active'); // 3
+	 content.classList.add('active'); // 4
+  });
+});
