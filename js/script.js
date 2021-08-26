12233ea2f92c48d0b4f60121aa57cb40cf4f6204
Andrey Varlamov <45011364+ivar1amov@users.noreply.github.com>
2021-08-26 03:05:29 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,51 +1,94 @@
-var swiper = new Swiper(".mySwiper", {
-   navigation: {
-     nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
-   },
+
+
+   var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+var swiperTop = new Swiper(".swiper-top", {
+	slidesPerView: 1, 
+	centeredSlides: true,  
+	navigation: {
+	     nextEl: ".swiper-button-next",
+	     prevEl: ".swiper-button-prev",
+	   },
+	 });
+	
+	
+	 var mySwiper = new Swiper('.swiper-top', {
+		speed: 400,
+		spaceBetween: 100
+  });
+
+	$('.js-tab-trigger').click(function() {
+		// клик! 
+ })
+ $('.js-tab-trigger').click(function() {
+	 var id = $(this).attr('data-tab'), // 1
+		  content = $('.js-tab-content[data-tab="'+ id +'"]'); // 2
+ });
+ 
+ $('.js-tab-trigger').click(function() {
+	 var id = $(this).attr('data-tab'),
+		  content = $('.js-tab-content[data-tab="'+ id +'"]');
+	 
+	 $('.js-tab-trigger.active').removeClass('active'); // 1
+	 $(this).addClass('active'); // 2
+	 
+	 $('.js-tab-content.active').removeClass('active'); // 3
+	 content.addClass('active'); // 4
+ });
+ 
+ 
+ var jsTriggers = document.querySelectorAll('.js-tab-trigger'),
+	  jsContents = document.querySelectorAll('.js-tab-content');
+	  
+	  jsTriggers.forEach(function(trigger) { // 1
+	 trigger.addEventListener('click', function() { // 2
+		 // клик!
+	 });
+ });
+ var id = this.getAttribute('data-tab'), // 1
+	  content = document.querySelector('.js-tab-content[data-tab="'+id+'"]'), // 2
+	  activeTrigger = document.querySelector('.js-tab-trigger.active'), // 3
+	  activeContent = document.querySelector('.js-tab-content.active'); // 4
+	  
+	  var jsTriggers = document.querySelectorAll('.js-tab-trigger'),
+	  jsContents = document.querySelectorAll('.js-tab-content');
+ 
+ jsTriggers.forEach(function(trigger) {
+	 trigger.addEventListener('click', function() {
+		 var id = this.getAttribute('data-tab'),
+			  content = document.querySelector('.js-tab-content[data-tab="'+id+'"]'),
+			  activeTrigger = document.querySelector('.js-tab-trigger.active'),
+			  activeContent = document.querySelector('.js-tab-content.active');
+		 
+		 activeTrigger.classList.remove('active'); // 1
+		 trigger.classList.add('active'); // 2
+		 
+		 activeContent.classList.remove('active'); // 3
+		 content.classList.add('active'); // 4
+	 });
  });
 
 
-//  window.onload = function() {
-//   var oQuotes = document.querySelector('.slogan__inner ul.slogan__list');
 
-//   /* Функция дублирования пунктов */
-//   function fDoublingQuotes() {
-//     let nElem = oQuotes.children.length;
-//     for (let i = 0; i < nElem; i++) {
-//       oQuotes.appendChild(oQuotes.children[i].cloneNode(true));
-//     }
-//     oQuotes.style.animationDuration = '30s,' + (nElem * 2) + 's';
-//   }
+ (() => {
+	const menuButton = document.querySelector('.menu__button');
+	const menuList = document.querySelector('.menu__list');
 
-
-
-//   fDoublingQuotes();
-// }
-
-
-
-
-
-$(document).ready(function() {
-  // Assign some jquery elements we'll need
-  var $swiper = $(".MySwipperTaste");
-  var $bottomSlide = null; // Slide whose content gets 'extracted' and placed
-  // into a fixed position for animation purposes
-  var $bottomSlideContent = null; // Slide content that gets passed between the
-  // panning slide stack and the position 'behind'
-  // the stack, needed for correct animation style
-
-  var mySwiper = new Swiper(".MySwipperTaste", {
-    spaceBetween: 30,
-    slidesPerView: 3,
-    centeredSlides: true,
-    roundLengths: true,
-    loop: true,
-    loopAdditionalSlides: 30,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    }
-  });
-});
+	menuButton.addEventListener('click', () => {
+		 let expanded = menuButton.getAttribute('aria-expanded') === 'true';
+		 menuButton.setAttribute('aria-expanded', !expanded);
+		 menuButton.classList.toggle('menu__button--open');
+		 menuList.classList.toggle('menu__list--open');
+	});
+})();
